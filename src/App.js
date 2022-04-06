cd03bd3f6a8a2a55c752ab239fb3046850129650
unknown <sanjayofficial755@gmail.com>
2022-04-06 23:26:43 +0530

    Add header component along with logo and two links i.e. shop and contact

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {Routes,Route} from "react-router-dom"
 import {HomePage} from "./pages/homePage/homePage"
 import {HatsPage} from "./pages/hatsPage/hatsPage"
+import { Header } from "./components/header/header";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Routes>
         <Route path="/" element ={<HomePage/>}/>
         <Route path="/hatsPage" element  ={<HatsPage/>}/>
---
+++ src/components/header/header.js
@@ -0,0 +1,20 @@
+import {Link} from 'react-router-dom'
+import {ReactComponent as Logo} from '../../assets/crown.svg'
+import './header.scss'
+export const Header = ()=>{
+    return(
+        <div className="header">
+                <Link className="logo-container" to="/">
+                    <Logo className="logo"/>
+                </Link>
+                <div className="options">
+                    <Link className="option" to="/hatsPage">
+                    Shop
+                    </Link>
+                    <Link className="option" to="/contact">
+                    Contact
+                    </Link>
+                </div>
+        </div>
+    )
+}
