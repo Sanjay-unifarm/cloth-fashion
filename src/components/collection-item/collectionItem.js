2dfecbcb17b1cc965268f9f74f1e8523361ac8ed
Sanjaykumar Gouda <sanjay.kumar@oropocket.com>
2023-03-12 12:35:55 +0530

    build push

--- src/components/collection-item/collectionItem.js
+++ src/components/collection-item/collectionItem.js
@@ -1,30 +1,29 @@
-import { CustomButton } from '../custom-button/customButton'
-import {addToCartAction} from '../../actions/addToCartAction'
-import {useDispatch,useSelector} from 'react-redux';
-import './collectionItem.scss'
+import { CustomButton } from "../custom-button/customButton";
+import { addToCartAction } from "../../actions/addToCartAction";
+import { useDispatch, useSelector } from "react-redux";
+import "./collectionItem.scss";
 
-export const CollectionItem = ({item})=>{
-  
-    const {name,price,imageUrl} = item
-    const dispatch = useDispatch();
-    
-    const handdleAddtoCart = (e)=>{
-        e.preventDefault();
-        dispatch(addToCartAction(item))
-    }
+export const CollectionItem = ({ item }) => {
+  const { name, price, imageUrl } = item;
+  const dispatch = useDispatch();
 
-    return(
-        <div className="collection-item">
-            <div
-                className="image"
-                style={{ backgroundImage:`url(${imageUrl})`}}
-            />
-            <div className="collection-footer">
-                <span className="name" >{name}</span>
-                <span  className="price">{price}</span>
-            </div>
-            <CustomButton className="custom-button" onClick={handdleAddtoCart}  btnLabelText="Add to Cart"/>
+  const handdleAddtoCart = (e) => {
+    e.preventDefault();
+    dispatch(addToCartAction(item));
+  };
 
-        </div>
-    )
-}
+  return (
+    <div className="collection-item">
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton
+        className="custom-button"
+        onClick={handdleAddtoCart}
+        btnLabelText="Add to Cart"
+      />
+    </div>
+  );
+};
